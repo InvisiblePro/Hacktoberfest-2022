245d1fcb4686e4b7f009d57be8de652e4d20a39a
mvisic <marcusvisic@yahoo.com.au>
2022-10-09 10:49:11 +0800

    Update hacktoberfest22-quiz.py

--- python/hacktoberfest22-quiz.py
+++ python/hacktoberfest22-quiz.py
@@ -26,7 +26,7 @@ if answer.lower()=='yes':
     else:
         print('Wrong Answer :(')
  
-print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
+print('Thankyou for Playing the Hacktoberfest quiz game, you attempted',score,"questions correctly!")
 mark=(score/total_questions)*100
 print('Marks obtained:',mark)
 print('BYE!')
