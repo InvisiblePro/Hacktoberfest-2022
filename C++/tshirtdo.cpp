63e0cb15ca2cf3b121fe5417c7871eab657c21ce
Ananay <ananaygupta23@gmail.com>
2022-10-11 08:07:58 +0530

    add

---
+++ C++/tshirtdo.cpp
@@ -0,0 +1,20 @@
+int numMatchingSubseq (string S, vector<string>& words) {
+		vector<vector<int>> alpha (26);
+		for (int i = 0; i < S.size (); ++i) alpha[S[i] - 'a'].push_back (i);
+		int res = 0;
+
+		for (const auto& word : words) {
+			int x = -1;
+			bool found = true;
+
+			for (char c : word) {
+				auto it = upper_bound (alpha[c - 'a'].begin (), alpha[c - 'a'].end (), x);
+				if (it == alpha[c - 'a'].end ()) found = false;
+				else x = *it;
+			}
+
+			if (found) res++;
+		}
+
+		return res;
+	}
