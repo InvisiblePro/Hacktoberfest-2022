ceec6293af4edf2486255ac1b8dcfa98afac28ef
Arindam Das <92679140+arindamdas13@users.noreply.github.com>
2022-10-18 11:25:44 +0530

    Create area_of_ellipse.java

---
+++ java/area_of_ellipse.java
@@ -0,0 +1,16 @@
+class ellipse
+{
+	static double area()
+	{
+		int a=5;
+		int b=7;
+		final double pi=3.14;
+		double c=pi*a*b;
+		return c;
+	}
+	public static void main(String[] args) 
+	{
+		double x=area();
+		System.out.println(x);
+	}
+}
