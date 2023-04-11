942b171a1a885787cc6b22f2cc19ebe5d81307e2
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-04-11 23:57:01 +0700

    feat: db initialization

--- Java/authservice/src/main/java/com/sinut/authservice/AuthserviceApplication.java
+++ Java/authservice/src/main/java/com/sinut/authservice/AuthserviceApplication.java
@@ -1,7 +1,13 @@
 package com.sinut.authservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sinut.authservice.models.Client;
+import com.sinut.authservice.models.Role;
+import com.sinut.authservice.service.ClientService;
 
 @SpringBootApplication
 public class AuthserviceApplication {
@@ -10,4 +16,26 @@ public class AuthserviceApplication {
 		SpringApplication.run(AuthserviceApplication.class, args);
 	}
 
+	@Bean
+	/**
+	 * Application Initialization
+	 * 
+	 * @param clientService
+	 * @return
+	 */
+	CommandLineRunner runner(ClientService clientService) {
+		return args -> {
+
+			clientService.saveUser(new Client("Sinut Wattanarporn", "cheesecake", "cheeeees"));
+			clientService.saveUser(new Client("Badamum John", "johnsnow", "dice1234"));
+
+			clientService.saveRole(new Role("ROOT"));
+			clientService.saveRole(new Role("GROUP 1"));
+			clientService.saveRole(new Role("GROUP 2"));
+
+			clientService.addRoleToUser("cheesecake", "ROOT");
+			clientService.addRoleToUser("johnsnow", "GROUP 1");
+			clientService.addRoleToUser("johnsnow", "GROUP 2");
+		};
+	}
 }
