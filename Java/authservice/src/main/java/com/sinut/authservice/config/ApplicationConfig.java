56cd2aa1eafad989c1dd51d9e388da0166b0b6ce
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-04-15 20:42:27 +0700

    update: app config

--- Java/authservice/src/main/java/com/sinut/authservice/config/ApplicationConfig.java
+++ Java/authservice/src/main/java/com/sinut/authservice/config/ApplicationConfig.java
@@ -2,7 +2,13 @@ package com.sinut.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.sinut.authservice.repository.ClientRepository;
 
@@ -18,6 +24,24 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         // Get UserDetails from Db
         return username -> clientRepository.findByUsername(username).orElseThrow();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
